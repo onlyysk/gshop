4a758d58ee38f02edf4f06649fe98b87993b2429
yangshaokai <onlyysk@163.com>
2018-11-20 11:03:07 +0800

    2

---
+++ src/store/actions.js
@@ -0,0 +1,31 @@
+
+import {
+  reqAddress,
+  reqShops,
+  reqFoodCategorys
+} from '../api'
+
+import {
+  RECEIVE_SHOPS,
+  RECEIVE_CATEGORYS,
+  RECEIVE_ADDRESS
+} from './mutation-types'
+
+export default {
+  async getAddress ({commit, state}) {
+    const {longitude, latitude} = state
+    const result = await reqAddress(longitude, latitude)
+    if(result.code===0) {
+      const address = result.data
+      commit(RECEIVE_ADDRESS, {address})
+    }
+  },
+  async getFoodCategorys({commit}){
+  const result = await reqFoodCategorys()
+  if(result.code===0){
+    const foodCategorys = result.data
+    commit(RECEIVE_CATEGORYS,{categorys:foodCategorys})
+  }
+},
+
+}
